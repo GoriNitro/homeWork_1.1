23b6a26cd6c744848b2735d315aa53ff4bc98260
GoriNitro <144053176+GoriNitro@users.noreply.github.com>
2023-09-28 19:19:08 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.net.SocketOptions;
+public class Main {
+    public static void main(String[] args) {
+        String MyFavouriteFood = "";
+        //п
+        int Num = 12;
+        //в
+
+        final String word = " nebula";
+        //т
+
+        MyFavouriteFood = (word + Num);
+        //ч
+
+        System.out.println(MyFavouriteFood + word +  Num);
+        //п
+
+        if (Num>=1) {
+            System.out.println("Вы сохранили положительное число");}
+        else if (Num==0) {
+            System.out.println("Вы сохранили ноль");}
+        else {
+            System.out.println("Вы сохранили отрицательное0 число");}
+        //ш
+        
+        }
+    }
